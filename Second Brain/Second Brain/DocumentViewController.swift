30f4b9f65c8e5877a343ba3ba733b90d46a44b0a
Ryan Goggins <ryanngoggins@gmail.com>
2018-02-05 23:33:52 -0500

    Update DocumentViewController.swift

--- Second Brain/Second Brain/DocumentViewController.swift
+++ Second Brain/Second Brain/DocumentViewController.swift
@@ -23,14 +23,57 @@ class DocumentViewController: UIViewController {
                 // Display the content of the document, e.g.:
                 self.documentNameLabel.text = self.document?.fileURL.lastPathComponent
             } else {
-                // Make sure to handle the failed import appropriately, e.g., by presenting an error message to the user.
+                // Make sure to handle the failed import appropriately, e.g., by presenting an error message to the user.c
             }
         })
+        
+        
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //Add in gesture commands
+        
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: Selector(("respondToSwipeGesture:")))
+        swipeRight.direction = UISwipeGestureRecognizerDirection.right
+        self.view.addGestureRecognizer(swipeRight)
+        
+        let swipeDown = UISwipeGestureRecognizer(target: self, action: Selector(("respondToSwipeGesture:")))
+        swipeDown.direction = UISwipeGestureRecognizerDirection.down
+        self.view.addGestureRecognizer(swipeDown)
+    }
+    
+    func respondToSwipeGesture(gesture: UIGestureRecognizer) {
+        //        Gesture recognizers for screen
+        if let swipeGesture = gesture as? UISwipeGestureRecognizer {
+            switch swipeGesture.direction {
+            case UISwipeGestureRecognizerDirection.right:
+                print("Swiped right")
+            case UISwipeGestureRecognizerDirection.down:
+                print("Swiped down")
+            case UISwipeGestureRecognizerDirection.left:
+                print("Swiped left")
+            case UISwipeGestureRecognizerDirection.up:
+                print("Swiped up")
+            default:
+                break
+            }
+        }
     }
     
     @IBAction func dismissDocumentViewController() {
         dismiss(animated: true) {
             self.document?.close(completionHandler: nil)
         }
+    }
+    
+    @IBAction func showMessage(sender: UIButton) {
+        let alertController = UIAlertController(title: "Welcome to the app", message:
+            "Hello world", preferredStyle: UIAlertControllerStyle.alert)
+        alertController.addAction(UIAlertAction(title: "OK", style:
+            UIAlertActionStyle.default, handler: nil))
+        present(alertController, animated: true, completion: nil)
     }
 }
